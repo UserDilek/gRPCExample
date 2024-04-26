838b2dc7d91aa86ea753f61140bd194aae87dbcd
UserDilek <dilekhocaoglu11@hotmail.com>
2024-04-26 16:38:42 -0400

    call frpc service from client

--- GrpcService1/ClientForgRPC/Program.cs
+++ GrpcService1/ClientForgRPC/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Grpc.Net.Client;
+using GrpcService1;
+using System.Linq.Expressions;
+
+Console.WriteLine("Hello from dilek");
+
+
+var channel = GrpcChannel.ForAddress("https://localhost:7024");
+var greeterClient = new Greeter.GreeterClient(channel);
+
+var clientResult = await greeterClient.SayHelloAsync(new HelloRequest { Name = "Example Name" });
+Console.WriteLine(clientResult.Message);
+
+
+
+
+Console.ReadLine();
+
+
+
+
+
---
+++ GrpcService1/GrpcService1/Services/DilekService.cs
@@ -0,0 +1,16 @@
+﻿using Grpc.Core;
+using GrpcService1;
+
+namespace GrpcService1.Services
+{
+    public class DilekService:Dilek.DilekBase
+    {
+        public override Task<DilekReply> HiDilek(DilekRequest request, ServerCallContext context)
+        {
+            return Task.FromResult(new DilekReply
+            {
+                Message = "Hello from Dilek"
+            });
+        }
+    }
+}
